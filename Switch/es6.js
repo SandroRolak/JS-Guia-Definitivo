a502141057491df4a0241370e4675726e34ff04b
SandroRolak <sandro.rolak2090@gmail.com>
2021-07-02 23:15:27 -0300

    Switch

---
+++ Switch/es6.js
@@ -0,0 +1,23 @@
+const sum = (firstParam, secondParam) => {
+    return firstParam + secondParam;
+};
+console.log(sum(2,5));
+
+const printHello = () => {
+    console.log('Hello');
+};
+printHello();
+
+const checkWeight = weight => {
+    console.log(`Baggage weight: ${weight} kilograms.`);
+};
+checkWeight(25);
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 30));
+
+function soma(num1, num2) {
+    return num1 + num2;
+}
+console.log(soma(3,6));
+
